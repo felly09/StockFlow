f2e6d4b8efc01cc1969655d49bf84951db7fb2ca
felly09 <shayofelicitas@gmail.com>
2026-05-16 15:43:45 +0530

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -18,20 +18,19 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
+def home(request):
+    return redirect('/accounts/login/')
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
-    path("dashboard/", include("analytics.urls")),
+    path('dashboard/', include('analytics.urls')),
     path('inventory/', include('inventory.urls')),
-    path("accounts/", include("accounts.urls")),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('allauth.urls')),
-
-
 ]
-    
-
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
